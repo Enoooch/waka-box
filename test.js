cc5c52ef06c080238491198d2e73ec05f7e63a98
Enoooch <tjc.enoch.c@gmail.com>
2021-12-07 10:09:26 +0800

    feat: preview text in test.js

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { WakaTimeClient, RANGE } = require("wakatime-client");
+const { unicodeProgressBar } = require("./utils.js");
 
 const wakatime = new WakaTimeClient(process.env.WAKATIME_API_KEY);
 
@@ -10,7 +11,13 @@ async function test() {
   for (let i = 0; i < Math.min(stats.data.languages.length, 10); i++) {
     const data = stats.data.languages[i];
     const { name, percent, text: time } = data;
-    console.log(`${name} - ${percent}% - ${time}`);
+    const line = [
+      name.padEnd(11),
+      time.padStart(14) + " ",
+      unicodeProgressBar(percent + 15),
+      String(percent.toFixed(1)).padStart(5) + "%"
+    ];
+    console.log(line.join(" "));
   }
 }
 
